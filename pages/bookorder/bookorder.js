d82dabdc0cbfcebf1d6445f2f807fcd25061460b
mc-feng <734568955@qq.com>
2018-08-20 18:03:46 +0800

    预约数据接口基本完成

--- pages/bookorder/bookorder.js
+++ pages/bookorder/bookorder.js
@@ -5,16 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-      data:null
+      data:null,
+      patientMessage:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let objects = JSON.parse(options.data);
-    console.log(objects);
-    this.setData({ data: objects });
+    if (options.data){
+      let objects = JSON.parse(options.data);
+      console.log(objects);
+      this.setData({ data: objects });
+    }else{
+      console.log(options)
+      let patientMessage = JSON.parse(options.patient);
+      console.log(patientMessage);
+      let objects = JSON.parse(options.nowData);
+      console.log(objects);
+      this.setData({ 
+        data: objects,
+        patientMessage: patientMessage
+      });
+    }
   },
 
   /**
@@ -67,6 +80,25 @@ Page({
   },
   // 弹出框
   showmode:function(e){
+   wx.request({
+     url: 'http://192.168.2.165:8081/booking/confirmbooking',
+     method:'post',
+     data:{
+       "hospt": this.data.data.trans.hospital,
+       "dept": this.data.data.trans.office,
+       "doc": this.data.data.trans.docName,
+       "cost": this.data.data.price,
+       "time": this.data.data.now + " " + this.data.data.time,
+       "resourceID": this.data.data.resourceid,
+       "hospitalUserID": this.data.patientMessage.openId,
+       "patientID": this.data.patientMessage.patientID,
+       "accessToken": "800EBED9-63E5-4408-A184-BE693DA32CB6",
+       "openUserID": "",
+     },
+     success:function(res){
+       console.log(res)
+     }
+   })
    this.showView()
   },
   showView:function(){
@@ -118,8 +150,9 @@ Page({
     })
   },
   linkToCard:function(){
+    let str = JSON.stringify(this.data.data)
     wx.navigateTo({
-      url: '../inHospital/inHospital',
+      url: '../inHospital/inHospital?data=' + str,
     })
   }
 })
--- pages/inHospital/inHospital.js
+++ pages/inHospital/inHospital.js
@@ -7,21 +7,44 @@ Page({
   data: {
     card:true,
     currentTab:0,
-    userName:""
+    userName:"",
+    identityCard:"",
+    phoneNumber:"",
+    medicareCard:"",
+    parameter: [{ id: 1 }, { id: 2}],//模拟数据(里面必须要对象先初始定义---不知道为什么)
+    transData:{},
+    data:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let objects = JSON.parse(options.data);
+    console.log(objects);
+    this.setData({ data: objects });
+    this.data.parameter[0].checked = true;
+    var that =this;
+    wx.request({
+      url: 'http://192.168.2.165:8081/medicalcard/getweachattopatient',
+      method: "post",
+      data: {
+        "openId": "123456"
+      },
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          parameter:res.data.result
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+   
   },
 
   /**
@@ -80,14 +103,82 @@ Page({
       })
     }
   },
-  getUserIdCardName:function(e){
+  // 参数点击响应事件（确认绑定那一张就诊卡）
+  parameterTap: function (e) {//e是获取e.currentTarget.dataset.id所以是必备的，跟前端的data-id获取的方式差不多
+    var that = this
+    var this_checked = e.currentTarget.dataset.id
+    var parameterList = this.data.parameter//获取Json数组
+    for (var i = 0; i < parameterList.length; i++) {
+      if (parameterList[i].openId == this_checked) {
+        parameterList[i].checked = true;//当前点击的位置为true即选中
+        //设置需要传输的数据
+        that.setData({
+          transData: parameterList[i]
+        })
+      }
+      else {
+        parameterList[i].checked = false;//其他的位置为false
+      }
+    }
+    that.setData({
+      parameter: parameterList
+    })
+    console.log(that.data.transData)
+  },
+  getUseruserName:function(e){
     this.setData({
       userName: e.detail.value
     })
+    console.log(this.data.userName)
+  },
+  getUseridentityCard: function (e) {
+    this.setData({
+      identityCard: e.detail.value
+    })
+    console.log(this.data.identityCard)
+  },
+  getUserphoneNumber: function (e) {
+    this.setData({
+      phoneNumber: e.detail.value
+    })
+    console.log(this.data.phoneNumber)
+  },
+  getUsermedicareCard: function (e) {
+    this.setData({
+      medicareCard: e.detail.value
+    })
+    console.log(this.data.medicareCard)
   },
   noadd:function(){
     this.setData({
       card: !this.data.card
+    })
+  },
+  addCard:function(){
+    wx.request({
+      url: 'http://192.168.2.165:8081/medicalcard/getRecordCard',
+      method:"post",
+      data:{
+        "openID":"123456",
+        "tel": this.data.phoneNumber,
+        "IDCard": this.data.identityCard,
+        "patientName": this.data.userName,
+        "cardNo": this.data.medicareCard,
+        "cardType": 0,
+        "cardProperty ": 0,
+        "accessToken": "800EBED9-63E5-4408-A184-BE693DA32CB6",
+        "openUserID": "",
+      },
+      success:function(res){
+        console.log(res)
+      }
+    })
+  },
+  ConfirmPatient:function(res){
+    let str = JSON.stringify(this.data.transData);
+    let str2 = JSON.stringify(this.data.data)
+    wx.navigateTo({
+      url: '../bookorder/bookorder?patient=' + str + '&nowData=' + str2,
     })
   }
 })
